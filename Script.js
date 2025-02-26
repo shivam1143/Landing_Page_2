b8428286f3824a37af7f077d7c599c59999c9b6e
Shivam1143 <shivam143600@gmail.com>
2025-02-27 00:37:16 +0530

    change some css code and add html code

--- Script.js
+++ Script.js
@@ -1,2 +1,15 @@
 const menuBtn = document.getElementById('menu-btn');
 const navlinks = document.getElementById('nav-links');
+const menuBtnIcon = menuBtn.querySelector('i');
+
+menuBtn.addEventListener('click', (e) => {
+    navlinks.classList.toggle('open');
+
+    const isOpen = navlinks.classList.contains('open');
+    menuBtnIcon.setAttribute('class', isOpen ? "ri-close-line" : "ri-menu-line");
+});
+
+navlinks.addEventListener('click', (e) => {
+    navlinks.classList.remove('open');
+    meniBtnIcon.setAttribute('class', "ri-menu-line");
+});
